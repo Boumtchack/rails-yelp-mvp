b6fe2c0503c0d1d2e38d2f74c9fae7b53f351920
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-02-21 15:25:44 +0100

    added reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
   get 'restaurants/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -9,6 +10,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :restaurants, only: %i[index show new create] do
-    resources :reviews, only: %i[new]
+    resources :reviews, only: %i[new create]
   end
 end
