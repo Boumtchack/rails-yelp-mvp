7fdaf2237c7806390bbf3c2b2a8b79147c173480
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-02-21 14:30:36 +0100

    add route for restaurents, seeds with 5 new restaurants, created restaurants controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.create(name: 'Le Baguette', address: 'Paris', category: 'french')
+Restaurant.create(name: 'Les Baguettes', address: 'Hong Kong', category: 'chinese')
+Restaurant.create(name: 'La Baguetta', address: 'Rome', category: 'italian')
+Restaurant.create(name: 'La Baguet', address: 'Bruxelles', category: 'belgian')
+Restaurant.create(name: 'Le Palais OOOOOOh', address: 'Les Ulis', category: 'japanese')
